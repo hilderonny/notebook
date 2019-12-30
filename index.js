393b27133ae9dc765c371fe3a69c264fb2618d43
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2019-12-30 22:34:00 +0100

    body-parser-Abhängigkeit eingebaut.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var localdb = require('@levelupsoftware/localdb');
+var bodyparser = require('body-parser');
 
 var notebook = {
 
@@ -11,6 +12,8 @@ var notebook = {
 
         // Module initialisieren
         await localdb.init(app);
+
+        app.use(bodyparser.json({limit: '10mb', extended: true})); // Bilder senden braucht soviel
 
         app.use('/api/notebook/book', require('./api/book')(express.Router(), db, auth));
         app.use('/api/notebook/page', require('./api/page')(express.Router(), db, auth));
